eb10d635f3a39bbbbf35a183cea122c0b5d6b31e
Harshit M <79850332+djharshit@users.noreply.github.com>
2024-10-18 17:42:07 +0530

    Refactor server.py to track employee and admin check-in times

--- server.py
+++ server.py
@@ -5,9 +5,42 @@ import requests
 from dotenv import load_dotenv
 from flask import Flask, jsonify, request
 
+# Variables
 load_dotenv()
+GOOGLE_CHAT_WEBHOOK_URL: str = environ.get("GOOGLE_CHAT_WEBHOOK_URL", "")
+THRESHOLD_TIME_STR = "10:15:00"
 
-GOOGLE_CHAT_WEBHOOK_URL = environ.get("GOOGLE_CHAT_WEBHOOK_URL", "")
+employee_list: list[str] = [
+    "Siva Munaga 227243",
+    "sainath Billadar 227242",
+    "Naresh Batta 227241",
+    "Divyang Dheer 227240",
+    "Aswin KS 227239",
+    "Harshit Mehra 227238",
+    "Hrithika Madireddy 227237",
+    "Aditi Padshala 227236",
+    "Shivakumar kummari 227235",
+    "Saikiran Bhushaboina 227229",
+    "Rohit Sai Katikireddy 227228",
+    "Maroju Brahmateja 227225",
+    "Varun Bukka 227224",
+    "Abbas Khan 227149",
+    "Shubham Singh 227175",
+    "Saikiran Bhushaboina 227229",
+    "Rohit Sai Katikireddy 227228",
+    "Maroju Brahmateja 227225",
+    "Varun Bukka 227224",
+    "Abbas Khan 227149",
+    "Shubham Singh 227175",
+]
+admin_list: list[str] = [
+    "Chinmay Sanghi CEO",
+    "CSM CSM Mentor",
+    "Ashish ashishjain MD",
+    "Manideep Singapaka 227234",
+]
+empl_ontime: list[str] = []
+admin_ontime: list[str] = []
 
 app = Flask(__name__)
 
@@ -29,23 +62,36 @@ def index():
 def workflow():
     form_data = request.form
 
-    emp_id = form_data.get("emp_id", "")
-    checkin_from_time = form_data.get("checkin_from_time", "")
+    emp_id: str = form_data.get("emp_id", "")
+    checkin_from_time: str = form_data.get("checkin_from_time", "")
 
-    threshold_time_str = "10:15:00"
-    threshold_time = datetime.strptime(threshold_time_str, "%H:%M:%S").time()
-
+    threshold_time = datetime.strptime(THRESHOLD_TIME_STR, "%H:%M:%S").time()
     checkin_time = datetime.strptime(checkin_from_time, "%d-%m-%Y %H:%M:%S").time()
 
     if checkin_time > threshold_time:
-        late_by = datetime.combine(datetime.min, checkin_time) - datetime.combine(
-            datetime.min, threshold_time
-        )
-        print(f"{emp_id} is late by {late_by}")
-        post_to_google_chat(f"{emp_id} is late by {late_by}")
+        pass
 
     else:
-        print(f"{emp_id} is on time at {checkin_time}")
+        if emp_id in employee_list:
+            empl_ontime.append(emp_id)
+            print(f"{emp_id} is on time at {checkin_time}")
+        elif emp_id in admin_list:
+            admin_ontime.append(emp_id)
+            print(f"{emp_id} is on time at {checkin_time}")
+
+    return jsonify({"message": "Success"})
+
+
+@app.route("/googlechat", methods=["GET"])
+def googlechat():
+    message = f"""Employees who have not yet checkin\n\n{'\n'.join(set(employee_list) - set(empl_ontime))}
+    \nAdmins who have not yet checkin\n\n{'\n'.join(set(admin_list) - set(admin_ontime))}"""
+
+    print(message)
+    post_to_google_chat(message)
+
+    empl_ontime.clear()
+    admin_ontime.clear()
 
     return jsonify({"message": "Success"})
 
